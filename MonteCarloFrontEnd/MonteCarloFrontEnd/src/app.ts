b262c98c320da38e7adce16753aec32bc128723b
Jack Girerd <jgirerd@pietech.com>
2018-07-24 15:06:05 -0400

    results page color change

--- MonteCarloFrontEnd/MonteCarloFrontEnd/src/app.ts
+++ MonteCarloFrontEnd/MonteCarloFrontEnd/src/app.ts
@@ -10,9 +10,10 @@ export default class App {
     config.map([
       { route: ['home'], name: 'home', moduleId: PLATFORM.moduleName('./home/home'), nav: true, title: 'Please Wait' },
       { route: ['','realHome'], name: 'realHome', moduleId: PLATFORM.moduleName('./realHome/realHome'), nav: true, title: 'Home' },
-      { route: ['input'], name: 'input', moduleId: PLATFORM.moduleName('./input/input'), nav: true, title: 'Input' },
+      { route: ['input'], name: 'input', moduleId: PLATFORM.moduleName('./input/input'), nav: true, title: 'homre' },
       { route: 'results', name: 'results', moduleId: PLATFORM.moduleName('./results/results'), nav: true, title: 'Results' },
       { route: 'portfolio', name: 'portfolio', moduleId: PLATFORM.moduleName('./portfolio/portfolio'), nav: true, title: 'Portfolio' },
+      { route: 'about', name: 'about', moduleId: PLATFORM.moduleName('./about/about'), nav: true, title: 'About' },
     ]);
     this.router = router;
   }
