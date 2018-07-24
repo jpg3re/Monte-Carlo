9c0a1e84bfe2390f8564991c3130c409984d96d1
Austin Sarmento <asarmento@pietech.com>
2018-07-24 13:27:07 -0400

    sorting distributions for front end

--- MonteCarlo/Models/Model/Output/OutputModel.cs
+++ MonteCarlo/Models/Model/Output/OutputModel.cs
@@ -16,7 +16,8 @@ namespace MonteCarlo.Models.Model
         private List<Task> rateTasks;
         private WeightRate[] weightRates;
         public List<string> names;
-        public List<Distributions> distributions;
+        public Distributions[] distributions;
+        private List<Distributions> allDistributions;
         private double[] historicalRates = { 9.68, 14.68, 10.71, 15.91, 9.32, 19.06, 7.06, 2.91, 9.01, 2.87, 5.84, 2.85, 5.88, 3.48, 0.18, 0.12, 2.32, 2.19 };
 
         public OutputModel(InputModel model)
@@ -26,11 +27,13 @@ namespace MonteCarlo.Models.Model
             this.model = model;
             weightRates = new WeightRate[amount * 2];
             names = new List<string>(amount);
-            rateTasks = new List<Task>(amount * 2);
-            distributions = new List<Distributions>(amount * 6);
+            rateTasks = new List<Task>(amount * 6);
+            allDistributions = new List<Distributions>(amount * 6);
+            distributions = new Distributions[amount * 6];
             MakeHistoricalAssets();
             Names();
             CalculateWeightRate();
+            SortDistributions();
         }
 
         private void Names()
@@ -58,16 +61,213 @@ namespace MonteCarlo.Models.Model
             }
         }
 
+        private void SortDistributions()
+        {
+            for (int i = 0; i < allDistributions.Count; i++) //that sweet sweet O(n) sorting algorithm
+            {
+                switch (amount)
+                {
+                    case 1:
+                        if (allDistributions[i].asset.name.Equals(names[0]))
+                        {
+                            switch (allDistributions[i].pdf)
+                            {
+                                case PDFType.Normal:
+                                    distributions[0] = allDistributions[i];
+                                    break;
+                                case PDFType.Laplace:
+                                    distributions[1] = allDistributions[i];
+                                    break;
+                                case PDFType.T:
+                                    distributions[2] = allDistributions[i];
+                                    break;
+                            }
+                        }
+                        else if (allDistributions[i].asset.name.Equals(names[0] + " historical"))
+                        {
+                            switch (allDistributions[i].pdf)
+                            {
+                                case PDFType.Normal:
+                                    distributions[3] = allDistributions[i];
+                                    break;
+                                case PDFType.Laplace:
+                                    distributions[4] = allDistributions[i];
+                                    break;
+                                case PDFType.T:
+                                    distributions[5] = allDistributions[i];
+                                    break;
+                            }
+                        }
+                        break;
+                    case 2:
+                        if (allDistributions[i].asset.name.Equals(names[0]))
+                        {
+                            switch (allDistributions[i].pdf)
+                            {
+                                case PDFType.Normal:
+                                    distributions[0] = allDistributions[i];
+                                    break;
+                                case PDFType.Laplace:
+                                    distributions[1] = allDistributions[i];
+                                    break;
+                                case PDFType.T:
+                                    distributions[2] = allDistributions[i];
+                                    break;
+                            }
+                        }
+                        else if (allDistributions[i].asset.name.Equals(names[0] + " historical"))
+                        {
+                            switch (allDistributions[i].pdf)
+                            {
+                                case PDFType.Normal:
+                                    distributions[3] = allDistributions[i];
+                                    break;
+                                case PDFType.Laplace:
+                                    distributions[4] = allDistributions[i];
+                                    break;
+                                case PDFType.T:
+                                    distributions[5] = allDistributions[i];
+                                    break;
+                            }
+                            distributions[1] = allDistributions[i];
+                        }
+                        else if (allDistributions[i].asset.name.Equals(names[1]))
+                        {
+                            switch (allDistributions[i].pdf)
+                            {
+                                case PDFType.Normal:
+                                    distributions[6] = allDistributions[i];
+                                    break;
+                                case PDFType.Laplace:
+                                    distributions[7] = allDistributions[i];
+                                    break;
+                                case PDFType.T:
+                                    distributions[8] = allDistributions[i];
+                                    break;
+                            }
+                        }
+                        else if (allDistributions[i].asset.name.Equals(names[1] + " historical"))
+                        {
+                            switch (allDistributions[i].pdf)
+                            {
+                                case PDFType.Normal:
+                                    distributions[9] = allDistributions[i];
+                                    break;
+                                case PDFType.Laplace:
+                                    distributions[10] = allDistributions[i];
+                                    break;
+                                case PDFType.T:
+                                    distributions[11] = allDistributions[i];
+                                    break;
+                            }
+                        }
+                        break;
+                    case 3:
+                        if (allDistributions[i].asset.name.Equals(names[0]))
+                        {
+                            switch (allDistributions[i].pdf)
+                            {
+                                case PDFType.Normal:
+                                    distributions[0] = allDistributions[i];
+                                    break;
+                                case PDFType.Laplace:
+                                    distributions[1] = allDistributions[i];
+                                    break;
+                                case PDFType.T:
+                                    distributions[2] = allDistributions[i];
+                                    break;
+                            }
+                        }
+                        else if (allDistributions[i].asset.name.Equals(names[0] + " historical"))
+                        {
+                            switch (allDistributions[i].pdf)
+                            {
+                                case PDFType.Normal:
+                                    distributions[3] = allDistributions[i];
+                                    break;
+                                case PDFType.Laplace:
+                                    distributions[4] = allDistributions[i];
+                                    break;
+                                case PDFType.T:
+                                    distributions[5] = allDistributions[i];
+                                    break;
+                            }
+                        }
+                        else if (allDistributions[i].asset.name.Equals(names[1]))
+                        {
+                            switch (allDistributions[i].pdf)
+                            {
+                                case PDFType.Normal:
+                                    distributions[6] = allDistributions[i];
+                                    break;
+                                case PDFType.Laplace:
+                                    distributions[7] = allDistributions[i];
+                                    break;
+                                case PDFType.T:
+                                    distributions[8] = allDistributions[i];
+                                    break;
+                            }
+                        }
+                        else if (allDistributions[i].asset.name.Equals(names[1] + " historical"))
+                        {
+                            switch (allDistributions[i].pdf)
+                            {
+                                case PDFType.Normal:
+                                    distributions[9] = allDistributions[i];
+                                    break;
+                                case PDFType.Laplace:
+                                    distributions[10] = allDistributions[i];
+                                    break;
+                                case PDFType.T:
+                                    distributions[11] = allDistributions[i];
+                                    break;
+                            }
+                        }
+                        else if (allDistributions[i].asset.name.Equals(names[2]))
+                        {
+                            switch (allDistributions[i].pdf)
+                            {
+                                case PDFType.Normal:
+                                    distributions[12] = allDistributions[i];
+                                    break;
+                                case PDFType.Laplace:
+                                    distributions[13] = allDistributions[i];
+                                    break;
+                                case PDFType.T:
+                                    distributions[14] = allDistributions[i];
+                                    break;
+                            }
+                        }
+                        else if (allDistributions[i].asset.name.Equals(names[2] + " historical"))
+                        {
+                            switch (allDistributions[i].pdf)
+                            {
+                                case PDFType.Normal:
+                                    distributions[15] = allDistributions[i];
+                                    break;
+                                case PDFType.Laplace:
+                                    distributions[16] = allDistributions[i];
+                                    break;
+                                case PDFType.T:
+                                    distributions[17] = allDistributions[i];
+                                    break;
+                            }
+                        }
+                        break;
+                }
+            }
+        }
+
         private void CalculateWeightRate()
         {
-            for(int element = 0; element < amount * 2; element++)
-            {
-                weightRates[element] = new WeightRate(assets[element]);
-                rateTasks.Add(Task.Run(() => { distributions.Add(new Distributions(assets[element], weightRates[element], PDFType.Normal, model)); }));
-                rateTasks.Add(Task.Run(() => { distributions.Add(new Distributions(assets[element], weightRates[element], PDFType.Laplace, model)); }));
-                rateTasks.Add(Task.Run(() => { distributions.Add(new Distributions(assets[element], weightRates[element], PDFType.T, model)); }));
-                Task.WaitAll(rateTasks.ToArray());
-            };           
+            Parallel.For(0, amount * 2, element =>
+              {
+                  weightRates[element] = new WeightRate(assets[element]);
+                  rateTasks.Add(Task.Run(() => { allDistributions.Add(new Distributions(assets[element], weightRates[element], PDFType.Normal, model)); }));
+                  rateTasks.Add(Task.Run(() => { allDistributions.Add(new Distributions(assets[element], weightRates[element], PDFType.Laplace, model)); }));
+                  rateTasks.Add(Task.Run(() => { allDistributions.Add(new Distributions(assets[element], weightRates[element], PDFType.T, model)); }));
+              });
+            Task.WaitAll(rateTasks.ToArray());
         }
     }
 }
